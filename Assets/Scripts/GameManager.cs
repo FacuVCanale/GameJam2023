8c5f5e9ce58691048b3facaa7b537ebd4e948965
frizynn <101231690+frizynn@users.noreply.github.com>
2023-11-04 23:17:38 -0300

    Juego

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance {get; private set; }
-    // Start is called before the first frame update
+    public static GameManager Instance { get; private set; }
+
     [SerializeField] private float initialScrollSpeed;
+    [SerializeField] private TMP_Text scoreText;
+
+
+    private bool isGameOver = false;
+
+    public AudioSource musicAudio;
+
     private float scrollSpeed;
     private float timer;
+    private float timer_for_score;
+    private float meters;
+    private float gameDuration = 5f; // Duración del juego en segundos 
+    
+    private float seconds_passed = 0f;
+
+
+    public GameObject John;
+
     private void Awake()
     {
-        if(Instance != null && Instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(this);
         }
@@ -20,16 +38,32 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        UpdateSpeed();
-        UpdateTime();
+        if (!isGameOver)
+        {
+            UpdateSpeed();
+            UpdateTime();
+            UpdateScore();
+            // Llamada al método para sumar los segundos
+            Debug.Log(seconds_passed);
+
+            // Verifica si el tiempo ha transcurrido y no se ha alcanzado la cantidad de metros necesaria
+            if (seconds_passed >= gameDuration && meters < 5000)
+            {
+                // El juego termina y se pierde
+                EndGame();
+            }
+        }
     }
 
     private void UpdateTime()
     {
+
         timer += Time.deltaTime;
+
+        seconds_passed+= Time.deltaTime;
     }
 
     public float GetScrollSpeed()
@@ -45,10 +79,58 @@ public class GameManager : MonoBehaviour
     private void UpdateSpeed()
     {
         float speedDivider = 8f;
-        scrollSpeed = initialScrollSpeed +  timer / speedDivider; 
+        scrollSpeed = initialScrollSpeed + timer / speedDivider;
         if (scrollSpeed > 10f)
-    	{
-         scrollSpeed = 10f;
-        } 
+        {
+            scrollSpeed = 10f;
+        }
     }
+
+    private void UpdateScore()
+    {
+        float scorePerSeconds = 3 + (scrollSpeed / 2f);
+
+        timer_for_score += Time.deltaTime;
+        meters = (int)(timer_for_score * scorePerSeconds);
+        scoreText.text = string.Format("Meters: {0:000000}", meters);
+    }
+
+    
+
+    
+
+  private void EndGame()
+  {
+
+    musicAudio.Stop();
+
+    isGameOver = true;
+
+    // Congela el tiempo 
+    Time.timeScale = 0f; 
+
+    
+
+    // Carga la escena de Game Over después de unos segundos
+    Invoke("LoadGameOverScene", 1f);
+  }
+
+  void LoadGameOverScene()
+  {
+    SceneManager.LoadScene("EndScene"); 
+  }
+
+
+  //AGREGAR PARA LA ESCENA DE MUERTE: 
+  /* Invoke("ReloadGameScene", 5f);
+}
+
+void ReloadGameScene()
+{
+  // Reinicia la música
+  musicAudio.Play(); 
+  
+  // Recarga la escena del juego
+  SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+} */
 }
