f70cb45e5247908843acbfe860b1f45574010407
FacuVCanale <facundovcanale@gmail.com>
2023-11-05 00:48:10 -0300

    Button Implemented

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-
-    public GameObject shadow;
-    public GameObject progress;
-    public GameObject Message;
-
-
+    public GameObject shadows;
     
-    public void Continue()
-    {
-        Destroy(GameObject.FindWithTag("TetrisPiece"));
-        
-        foreach (Transform child in Message.transform)
-        {
-            child.gameObject.SetActive(false);
+    private int shadowIndex = 0;
+    private List<GameObject> shadowList;
+
+
+    void Start() {
+        // Get an array of all the child GameObjects
+        Transform[] shadowsChildren = shadows.GetComponentsInChildren<Transform>();
+        foreach (Transform shadow in shadowsChildren) {
+            shadowList.Add(shadow.gameObject);
+        }
+    }
+
+    public void OnButtonPress() {
+        GameObject TetrisToDelete = GameObject.FindWithTag("TetrisPiece");
+        while (TetrisToDelete != null) {
+            Destroy(TetrisToDelete);
+            TetrisToDelete = GameObject.FindWithTag("TetrisPiece");
         }
 
-        
-        Destroy(shadow.transform.GetChild(0).gameObject);
-        shadow.transform.GetChild(0).gameObject.SetActive(true);
-
-        
-        Destroy(progress.transform.GetChild(0).gameObject);
-        progress.transform.GetChild(0).gameObject.SetActive(true);
+        if (shadowIndex >= shadowList.Count) {
+            shadowIndex = 0;
+        }
+        if (shadowIndex > 0) {
+            shadowList[shadowIndex-1].SetActive(false);
+        }
+        shadowList[shadowIndex].SetActive(true);
+        shadowIndex += 1;
     }
     
 }
